180617cd6743180e4b05f727b8dfad45d7bd1a43
somayyah <somayyahmohd95@gmail.com>
2021-02-11 13:43:22 +0000

    typescript setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const path = require('path');
+const program = require('commander');
+
+clear();
+console.log(
+  chalk.red(
+    figlet.textSync('coral-reef-CLI', { horizontalLayout: 'full' })
+  )
+);
