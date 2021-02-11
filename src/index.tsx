e279b6111a4e630bf7615313dada09502320c5ed
somayyah <somayyahmohd95@gmail.com>
2021-02-11 14:25:49 +0000

    CLI skelton

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const path = require('path');
-const program = require('commander');
+const inquirer = require('inquirer');
 
 clear();
 console.log(
@@ -12,3 +12,18 @@ console.log(
     figlet.textSync('coral-reef-CLI', { horizontalLayout: 'full' })
   )
 );
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
