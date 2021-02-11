a5b91d826499e2ca4b149c41154b1944d5065892
somayyah <somayyahmohd95@gmail.com>
2021-02-11 11:20:43 +0000

    enquirer added

--- index.tsx
+++ index.tsx
@@ -1,1 +1,14 @@
-console.log("hello world!!")
+#!/usr/bin/env node
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const path = require('path');
+const program = require('enquirer');
+
+clear();
+console.log(
+  chalk.red(
+    figlet.textSync('Coral-CLI', { horizontalLayout: 'full' })
+  )
+);
+
